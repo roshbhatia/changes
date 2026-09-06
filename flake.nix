58a7461f5960a09bc3a4a1e66c714c943919d762
Roshan Bhatia <rshnbhatia@gmail.com>
2026-09-06 11:48:29 -0700

    chore(release): prepare v0.8.0

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          version = "0.7.0";
+          version = "0.8.0";
           mkPackage =
             {
               name,
